1ca8bbaf1afd0d379c5815ab5f58291d5ef6001d
Fatima <fawicoma@gmail.com>
2023-10-12 21:27:22 -0500

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Discover the best of Studio Ghibli</h1>
+    </>
+  )
+}
+
+export default App
